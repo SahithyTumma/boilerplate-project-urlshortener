1ae77d1a7eb215f081d858c6d668ef58d156352f
Sahithy Tumma <200010054@iitdh.ac.in>
2024-02-22 18:34:47 +0000

    done

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +23,24 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urls = {}
+
+app.post('/api/shorturl', function(req, res) {
+  const url = req.body.url;
+  console.log(url);
+  // console.log(req.url);
+  const shorturl = 1;
+  urls[shorturl] = url;
+  // console.log(urls);
+  res.json({original_url: url, short_url: shorturl});
+})
+
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  const short_url = req.params.shortUrl;
+  const original_url = urls[short_url];
+  res.redirect(original_url);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
